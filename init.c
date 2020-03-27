4002c71a459a21b02d873b90e3969fadfe04d09b
Richard Russon <rich@flatcap.org>
2020-03-27 10:58:49 +0000

    refactor slist use

--- init.c
+++ init.c
@@ -1582,9 +1582,9 @@ int charset_validator(const struct ConfigSet *cs, const struct ConfigDef *cdef,
   if (value == 0)
     return CSR_SUCCESS;
 
-  const char *str = (const char *) value;
+  const struct Slist *list = (const struct Slist *) value;
 
-  if ((strcmp(cdef->name, "charset") == 0) && strchr(str, ':'))
+  if ((strcmp(cdef->name, "charset") == 0) && (list->count > 1))
   {
     mutt_buffer_printf(
         err, _("'charset' must contain exactly one character set name"));
@@ -1592,24 +1592,36 @@ int charset_validator(const struct ConfigSet *cs, const struct ConfigDef *cdef,
   }
 
   int rc = CSR_SUCCESS;
-  bool strict = (strcmp(cdef->name, "send_charset") == 0);
-  char *q = NULL;
-  char *s = mutt_str_dup(str);
-
-  for (char *p = strtok_r(s, ":", &q); p; p = strtok_r(NULL, ":", &q))
+  struct ListNode *np = NULL;
+  STAILQ_FOREACH(np, &list->head, entries)
   {
-    if (*p == '\0')
-      continue;
-    if (!mutt_ch_check_charset(p, strict))
-    {
-      rc = CSR_ERR_INVALID;
-      mutt_buffer_printf(err, _("Invalid value for option %s: %s"), cdef->name, p);
+    rc = single_charset_validator(cs, cdef, (intptr_t) np->data, err);
+    if (rc != CSR_SUCCESS)
       break;
-    }
   }
 
-  FREE(&s);
   return rc;
+}
+
+/**
+ * single_charset_validator - Validates a single charset variable - Implements ConfigDef::validator()
+ */
+int single_charset_validator(const struct ConfigSet *cs, const struct ConfigDef *cdef,
+                             intptr_t value, struct Buffer *err)
+{
+  if (value == 0)
+    return CSR_SUCCESS;
+
+  const char *charset = (const char *) value;
+  const bool strict = (strcmp(cdef->name, "send_charset") == 0);
+
+  if (!mutt_ch_check_charset(charset, strict))
+  {
+    mutt_buffer_printf(err, _("Invalid value for option %s: %s"), cdef->name, value);
+    return CSR_ERR_INVALID;
+  }
+
+  return CSR_SUCCESS;
 }
 
 #ifdef USE_HCACHE
